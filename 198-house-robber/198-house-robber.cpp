bc5ae47287d930af7570ddc81e6afb39d198c8f7
VinodPangul <vinodpangul2@gmail.com>
2022-01-05 22:36:11 -0800

    Time: 0 ms (100.00%), Space: 7.7 MB (71.20%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,3 +1,4 @@
+#if 0
 class Solution {
 public:
     int helper(vector<int> &nums, int idx) {
@@ -11,9 +12,52 @@ public:
     }
     
     int rob(vector<int>& nums) {
-        
         return helper(nums, 0);
     }
 private:
     map<int, int> cache;
 };
+#endif
+
+#if 0
+//  DP version, O(n) & O(n)
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        int profit = 0;
+        if(n==0) return profit;
+        
+        vector<int> dp(n+1, 0);
+        dp[n] = 0;
+        dp[n-1] = nums[n-1];
+        for(int i=n-2; i>=0 ; i--) {
+            dp[i] = max(nums[i]+dp[i+2], dp[i+1]);
+        }
+        return dp[0];
+    }
+};
+#endif
+
+#if 1
+// Optimized DP version, O(n) & O(1)
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        int profit = 0;
+        if(n==0) return profit;
+        
+        int nextHomePlusOne = 0;
+        int nextHome = nums[n-1];
+        int currentMax = nums[n-1];
+        for(int i=n-2; i>=0 ; i--) {
+            currentMax = max(nums[i]+nextHomePlusOne, nextHome);
+            nextHomePlusOne = nextHome;
+            nextHome = currentMax;
+            
+        }
+        return currentMax;
+    }
+};
+#endif
