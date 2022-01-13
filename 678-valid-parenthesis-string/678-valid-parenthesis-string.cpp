7e6142c4c8cbcca1e6869590409b18f60d51e2f0
VinodPangul <vinodpangul2@gmail.com>
2022-01-12 19:49:26 -0800

    Time: 0 ms (100.00%), Space: 6.1 MB (53.75%) - LeetHub

--- 678-valid-parenthesis-string/678-valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/678-valid-parenthesis-string.cpp
@@ -1,3 +1,4 @@
+#if 0
 class Solution {
 public:
     bool checkValidString(string s) {
@@ -37,8 +38,44 @@ public:
         return st.empty();
     }
 };
+#endif
 
+#if 1
+// Mathematical Approach
+// https://leetcode.com/problems/valid-parenthesis-string/discuss/543521/Java-Count-Open-Parenthesis-O(n)-time-O(1)-space-Picture-Explain
+class Solution {
+public:
+ bool checkValidString(string s) {
+    int cmax=0, cmin=0;
+    for(int i=0; i<s.size(); i++) {
+        switch(s[i]) {
+            case '(':
+                cmax++; 
+                cmin++;
+                break;
+            
+            case ')':
+                cmax--;
+                cmin--;
+                break;
+            
+            case '*':
+                cmax++;
+                cmin--;
+                break;
+            
+            default:
+                return false;
+        }
+        
+        if(cmax<0) return false;
+         cmin = max(cmin, 0);
+    }
+    
+    return cmin==0;
+ }
+};
+#endif
 
-// "((**(*" --> n = 6
     
 
