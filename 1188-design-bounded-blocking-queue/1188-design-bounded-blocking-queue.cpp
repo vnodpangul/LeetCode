fe284b48c20305fc937640abf01580a05cc57e99
VinodPangul <vinodpangul2@gmail.com>
2022-01-12 22:02:26 -0800

    Time: 140 ms (16.85%), Space: 8.6 MB (86.03%) - LeetHub

--- 1188-design-bounded-blocking-queue/1188-design-bounded-blocking-queue.cpp
+++ 1188-design-bounded-blocking-queue/1188-design-bounded-blocking-queue.cpp
@@ -1,8 +1,3 @@
-#include <mutex>
-#include <condition_variable>
-#include <thread>
-#include <queue>
-#include <deque>
 
 class BoundedBlockingQueue {
 public:
@@ -12,33 +7,34 @@ public:
     
     void enqueue(int element) {
         unique_lock<mutex> lock(_mtx);
-        _cond.wait(lock, [this](){
+        _notify_for_enque.wait(lock, [this](){
             return _q.size()<_maxSize;
         });
         
         _q.push_front(element);
         lock.unlock();
-        _cond.notify_all();
+        _notify_for_deque.notify_one(); // Notify only one thread to avoid waking up all threads
     }
     
     int dequeue() {
         unique_lock<mutex> lock(_mtx);
-        _cond.wait(lock, [this](){
+        _notify_for_deque.wait(lock, [this](){
             return _q.size()>0;
         });
         int element = _q.back(); _q.pop_back();
         
         lock.unlock();
-        _cond.notify_all();
+        _notify_for_enque.notify_one();
         return element; 
     }
     
     int size() {
+        lock_guard<mutex> lock(_mtx);
         return _q.size();
     }
 private:
     deque<int> _q;
     mutex _mtx;
-    condition_variable _cond;
+    condition_variable _notify_for_deque, _notify_for_enque; // Use two condition variables to avoid starvation
     int _maxSize = 0;
 };
