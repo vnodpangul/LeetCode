f3be4d65f2df6584d6f2f81b8c36ec6477dd217e
VinodPangul <vinodpangul2@gmail.com>
2022-02-03 22:35:00 -0800

    Time: 104 ms (98.31%), Space: 64.7 MB (58.80%) - LeetHub

---
+++ 938-range-sum-of-bst/938-range-sum-of-bst.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void helper(TreeNode *root, int low, int high, int &sum)
+    {
+        if(!root) return;
+        if(root->val < low) return helper(root->right, low, high, sum);
+        if(root->val > high) return helper(root->left, low, high, sum);
+        
+        sum += root->val;
+        helper(root->left, low, high, sum);
+        helper(root->right, low, high, sum);
+    }
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        int sum = 0;
+        helper(root, low, high, sum);
+        return sum;
+    }
+};
