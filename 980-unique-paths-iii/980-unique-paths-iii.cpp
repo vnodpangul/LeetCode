78adddb6aec9d88271da628f27e2ec7f4a54e86e
VinodPangul <vinodpangul2@gmail.com>
2022-03-19 17:14:29 -0700

    Time: 6 ms (42.96%), Space: 7.1 MB (32.04%) - LeetHub

--- 980-unique-paths-iii/980-unique-paths-iii.cpp
+++ 980-unique-paths-iii/980-unique-paths-iii.cpp
@@ -1,52 +1,53 @@
 class Solution {
 public:
     
-    void DFS(vector<vector<int>>& grid, int x, int y, int &res, int empty)
+    void DFS(vector<vector<int>>& grid, int m, int n, int x, int y,
+            int countZero, vector<vector<bool>> &visited, int &res)
     {
-        if(x<0 || x>=grid.size() || y<0 || y>= grid[0].size() || grid[x][y]<0)
-            return;
-
-        if(grid[x][y]==2) {
-            if(empty==0)
-                res++;
+        if(x<0 || y<0 || x>=m || y>=n || grid[x][y] == -1 || 
+          visited[x][y])
+        {
             return;
         }
         
-        grid[x][y] = -2;
-        empty--;
-        DFS(grid, x+1, y, res, empty);
-        DFS(grid, x-1, y, res, empty);
-        DFS(grid, x, y+1, res, empty);
-        DFS(grid, x, y-1, res, empty);
-        grid[x][y] = 0;
-        empty++;
+        if(grid[x][y] == 2) {
+            if(countZero==0)
+                res++;
+            return;
+        }
+        visited[x][y] = true;
+        --countZero;
+        DFS(grid, m, n, x+1, y,  countZero, visited, res);
+        DFS(grid, m, n, x-1, y, countZero, visited, res);
+        DFS(grid, m, n, x, y+1, countZero, visited, res);
+        DFS(grid, m, n, x, y-1, countZero, visited, res);
+        visited[x][y] = false;
+        ++countZero;
+        
     }
     
     int uniquePathsIII(vector<vector<int>>& grid) {
         
         int m = grid.size();
-        if(m==0) return 0;
         int n = grid[0].size();
         
-        int empty=0;
+        int start_x = -1, start_y = -1;
+        int countZero = 1;
         for(int i=0; i<m; i++) {
             for(int j=0; j<n; j++) {
-               if(grid[i][j]==1) {
-                    startx = i;
-                    starty = j;
-                }else if(grid[i][j] != -1){
-                    empty++;
+                if(grid[i][j]==1) {
+                    start_x = i;
+                    start_y = j;
+                }else if(grid[i][j] == 0) {
+                    countZero++;
                 }
             }
         }
         
-        int res=0;
-      
-        // cout<<"Empty: "<<empty<<endl;
-        DFS(grid, startx, starty, res,  empty);
+        int res = 0;
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
+        DFS(grid, m, n, start_x, start_y, countZero, visited, res);
         return res;
+        
     }
-    
-    private:
-        int endx=0, endy=0, startx=0, starty=0;
 };
