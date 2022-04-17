d75b9f2530cdfe9fd5860ea85a8893b9b2f2dc78
VinodPangul <vinodpangul2@gmail.com>
2022-04-17 10:19:46 -0700

    Time: 42 ms (83.36%), Space: 18.7 MB (16.54%) - LeetHub

--- 1476-subrectangle-queries/1476-subrectangle-queries.cpp
+++ 1476-subrectangle-queries/1476-subrectangle-queries.cpp
@@ -1,29 +1,31 @@
 class SubrectangleQueries {
 public:
     SubrectangleQueries(vector<vector<int>>& rectangle) {
-        int m = rectangle.size();
-        int n = rectangle[0].size();
-        rect.resize(m, vector<int>(n, 0));
-        for(int i=0; i<m; i++) {
-            for(int j=0; j<n; j++) {
-                rect[i][j] = rectangle[i][j];
-            }
-        }
+        rect = rectangle;
     }
     
     void updateSubrectangle(int row1, int col1, int row2, int col2, int newValue) {
-        for(int i=row1; i<=row2; i++) {
-            for(int j=col1; j<=col2; j++) {
-                rect[i][j] = newValue;
-            }
-        }
+        history.push_back(Node(row1,col1,row2,col2, newValue));
     }
     
     int getValue(int row, int col) {
-        return rect[row][col];
+        for(int i=history.size()-1; i>=0; i--) {
+            Node n = history[i];
+            if(row>=n.x1 && col >=n.y1 && row<=n.x2 && col<=n.y2) {
+                return n.val;
+            }
+           
+        }
+         return rect[row][col];
     }
     private:
-        vector<vector<int>> rect;
+    struct Node {
+        int x1,y1,x2,y2;
+        int val;
+        Node (int _x1, int _y1, int _x2, int _y2, int _val) : x1(_x1), y1(_y1), x2(_x2), y2(_y2), val(_val) {}; 
+    };
+    vector<vector<int>> rect;
+    vector<Node> history;
 };
 
 /**
@@ -32,8 +34,3 @@ public:
  * obj->updateSubrectangle(row1,col1,row2,col2,newValue);
  * int param_2 = obj->getValue(row,col);
  */
-
-// 1 2 1
-// 4 3 4
-// 3 2 1
-// 1 1 1
