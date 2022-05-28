1413211be87c16f234645532c6eed23ec8b463cf
VinodPangul <vinodpangul2@gmail.com>
2022-05-28 13:16:00 -0700

    Time: 0 ms (100.00%), Space: 7.2 MB (64.39%) - LeetHub

--- 140-word-break-ii/140-word-break-ii.cpp
+++ 140-word-break-ii/140-word-break-ii.cpp
@@ -1,37 +1,31 @@
 class Solution {
 public:
-    
-    void helper(string s, int start, string str)
+    void helper(string &s, int start, string str)
     {
         if(start == s.size()) {
-            rc.insert(str);
+            res.push_back(str);
             return;
         }
-        
         for(int end=start+1; end<=s.size(); end++) {
             string tmp = s.substr(start, end-start);
-            if(wordDictSet.count(tmp)>0) {
-                helper(s, end, str==""?str+tmp:str+" "+tmp);
-            } 
-        }
+            if(dict.count(tmp)>0) {
+                helper(s, end, str==""? str+tmp: str+" "+tmp);
+            }
             
+        }
         
     }
     
     vector<string> wordBreak(string s, vector<string>& wordDict) {
         
         for(auto w: wordDict)
-            wordDictSet.insert(w);
+            dict.insert(w);
         
-        vector<string> rc_vec;
-        string tmp="";
+        string tmp = "";
         helper(s, 0, tmp);
-        for(auto s: rc)
-            rc_vec.push_back(s);
-
-        return rc_vec;
+        return res;
     }
     private:
-        set<string> wordDictSet;
-    set<string> rc; 
+        set<string> dict;
+        vector<string> res;
 };
