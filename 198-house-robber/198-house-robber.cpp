d38c4a8cd9f64695d17d410da292dc8553466f16
VinodPangul <vinodpangul2@gmail.com>
2022-06-04 20:53:51 -0700

    Time: 4 ms (32.65%), Space: 8.1 MB (6.43%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,63 +1,28 @@
-#if 0
 class Solution {
 public:
-    int helper(vector<int> &nums, int idx) {
-        if(idx>= nums.size())
-            return 0;
-        if(cache.count(idx))
-            return cache[idx];
-        int total1= nums[idx] + helper(nums, idx+2);
-        int total2 = helper(nums, idx+1);
-        return cache[idx] = max(total1, total2);
+    // [1,2,3,1]
+    
+    int helper(vector<int> &nums, int i, map<int, int> &dp) {
+        if(dp.count(i))
+            return dp[i];
+        else if(i==0)
+            return dp[i]=nums[i];
+        else if(i==1)
+            return dp[i] = max(nums[i], nums[i-1]);
+        else
+            return dp[i] = max( (nums[i]+helper(nums, i-2, dp)), helper(nums, i-1, dp) ); // 0+
+        
     }
     
     int rob(vector<int>& nums) {
-        return helper(nums, 0);
-    }
-private:
-    map<int, int> cache;
-};
-#endif
-
-#if 0
-//  DP version, O(n) & O(n)
-class Solution {
-public:
-    int rob(vector<int>& nums) {
-        int n = nums.size();
-        int profit = 0;
-        if(n==0) return profit;
+        /* Top Down implementation */
         
-        vector<int> dp(n+1, 0);
-        dp[n] = 0;
-        dp[n-1] = nums[n-1];
-        for(int i=n-2; i>=0 ; i--) {
-            dp[i] = max(nums[i]+dp[i+2], dp[i+1]);
-        }
-        return dp[0];
-    }
-};
-#endif
-
-#if 1
-// Optimized DP version, O(n) & O(1)
-class Solution {
-public:
-    int rob(vector<int>& nums) {
+        int res = INT_MIN;
         int n = nums.size();
-        int profit = 0;
-        if(n==0) return profit;
+        map<int, int> dp;
         
-        int nextHomePlusOne = 0;
-        int nextHome = nums[n-1];
-        int currentMax = nums[n-1];
-        for(int i=n-2; i>=0 ; i--) {
-            currentMax = max(nums[i]+nextHomePlusOne, nextHome);
-            nextHomePlusOne = nextHome;
-            nextHome = currentMax;
-            
-        }
-        return currentMax;
+        res = helper(nums, nums.size()-1, dp);
+        return res;
+        
     }
 };
-#endif
