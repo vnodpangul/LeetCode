26848baeda48be11f16abc5a4035f36381bc6886
VinodPangul <vinodpangul2@gmail.com>
2022-07-03 18:07:57 -0700

    Time: 344 ms (58.86%), Space: 87.9 MB (84.50%) - LeetHub

--- 1146-snapshot-array/1146-snapshot-array.cpp
+++ 1146-snapshot-array/1146-snapshot-array.cpp
@@ -1,24 +1,26 @@
 class SnapshotArray {
+    map<int, map<int, int>> cache;
+    int snapId = 0;
+    int maxLen = 0;
 public:
     SnapshotArray(int length) {
+        maxLen = length;
     }
     
     void set(int index, int val) {
-        snapMp[index][snapCount] = val;
+        cache[index][snapId] = val;
     }
     
     int snap() {
-        return snapCount++;
+        snapId++;
+        return snapId-1;
     }
     
     int get(int index, int snap_id) {
-        auto it = snapMp[index].upper_bound(snap_id);
-        if(it == snapMp[index].begin()) return 0;
+        auto it = cache[index].upper_bound(snap_id);
+        if(it == cache[index].begin()) return 0;
         return prev(it)->second;
     }
-private:
-    int snapCount = 0;
-    unordered_map<int, map<int, int>> snapMp; //<idx, <snapId, val>>
 };
 
 /**
@@ -28,3 +30,8 @@ private:
  * int param_2 = obj->snap();
  * int param_3 = obj->get(index,snap_id);
  */
+
+// map[index][snap_id] map;
+
+// map<int, map<int, int>> cache;
+
