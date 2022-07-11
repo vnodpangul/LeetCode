197d9d1d9621a07bd2527fda39c16eda89b34315
VinodPangul <vinodpangul2@gmail.com>
2022-07-11 12:51:03 -0700

    Time: 11 ms (70.25%), Space: 9.7 MB (57.42%) - LeetHub

---
+++ 256-paint-house/256-paint-house.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minCost(vector<vector<int>>& costs) {
+        int n = costs.size();
+        
+        for(int i=1; i<n; i++)
+        {
+            // Red Color
+            costs[i][0] += min(costs[i-1][1], costs[i-1][2]);
+            
+            // Blue Color
+            costs[i][1] += min(costs[i-1][0], costs[i-1][2]);
+            
+            // Green Color
+            costs[i][2] += min(costs[i-1][0], costs[i-1][1]);
+        }
+        
+        return min(costs[n-1][0], min(costs[n-1][1], costs[n-1][2]));
+        
+        
+    }
+};
+
