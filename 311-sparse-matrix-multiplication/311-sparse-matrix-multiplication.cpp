01c9732e3fe14c565fe893db430fce630bfc46a8
VinodPangul <vinodpangul2@gmail.com>
2022-07-21 21:47:42 -0700

    Time: 6 ms (74.59%), Space: 8.2 MB (49.59%) - LeetHub

--- 311-sparse-matrix-multiplication/311-sparse-matrix-multiplication.cpp
+++ 311-sparse-matrix-multiplication/311-sparse-matrix-multiplication.cpp
@@ -1,27 +1,32 @@
+/*
+Brute Force
+TC: O(n*k*m)
+SC: O(1)
+*/
 class Solution {
 public:
     vector<vector<int>> multiply(vector<vector<int>>& mat1, vector<vector<int>>& mat2) {
-        int r1 = mat1.size(), c1 = mat1[0].size(), r2=mat2.size(), c2=mat2[0].size();
         
-        vector<vector<int>> mat2Cols(r2, vector<int>());
-        for(int i=0; i<r2; i++) {
-            for(int j=0; j<c2; j++) {
-                if(mat2[i][j] != 0) 
-                    mat2Cols[i].push_back(j);
+        int m = mat1.size();
+        int kk = mat1[0].size();
+        int n = mat2[0].size();
+        
+        vector<vector<int>> res(m, vector<int>(n,0));
+        for(int i=0; i<m; i++)
+        {
+            for(int k=0; k<kk; k++)
+            {
+                if(mat1[i][k]!=0)
+                {
+                    for(int j=0; j<n; j++)
+                    {
+                        res[i][j] += mat1[i][k] * mat2[k][j];
+                        
+                    }
+                }                
             }
+
         }
-        
-        vector<vector<int>> res(r1, vector<int>(c2, 0));
-        for(int i=0; i<r1; i++) {
-            for(int j=0; j<c1; j++) {
-                if(mat1[i][j] != 0) {
-                    
-                    for(int k: mat2Cols[j]) 
-                        res[i][k] += (mat1[i][j]*mat2[j][k]);
-                }
-            }
-        }
-        
         return res;
     }
 };
