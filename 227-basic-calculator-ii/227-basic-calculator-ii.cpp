aa71c731b9048f0c92d9b418dcf68a7a2a4bc55a
VinodPangul <vinodpangul2@gmail.com>
2022-08-04 19:53:07 -0700

    Time: 20 ms (34.75%), Space: 8.6 MB (46.61%) - LeetHub

--- 227-basic-calculator-ii/227-basic-calculator-ii.cpp
+++ 227-basic-calculator-ii/227-basic-calculator-ii.cpp
@@ -1,59 +1,63 @@
 class Solution {
+    stack<int> st;
+    long num=0;
+    char sign = '+';
 public:
     int calculate(string s) {
-        int res = 0;
-        stack<int> st;
         
-        char sign = '+', nextSign = '+';
-        int i=0; int num = 0;
-        while(i<s.size()) {
-            while(isspace(s[i])) i++;
-            while(isdigit(s[i]) && i<s.size()) {
-                num = num*10 + (s[i]-'0');
+        int i=0; 
+        while(i<s.size())
+        {
+            while(i<s.size() && s[i]==' ') i++;
+            while(isdigit(s[i]))
+            {
+                num = num*10 + s[i]-'0';
                 i++;
             }
-            if(i<s.size() && isspace(s[i])) i++;
-            nextSign = s[i];
+            while(i<s.size() && s[i]==' ') i++;
+            char ch = s[i]; i++;
+
             
-            int tmp = 0;
-            switch(sign) {
-                    case '+':
-                        st.push(num);
-                        sign = nextSign;
-                        num = 0;
-                        break;
-                    case '-':
-                        st.push(-1* num);
-                        sign = nextSign;
-                        num = 0;
-                        break;
-                    case '*':
-                        tmp = st.top(); st.pop();
-                        st.push(tmp*num);
-                        sign = nextSign;
-                        num = 0;
-                        break;
-                    case '/':
-                        tmp = st.top(); st.pop();
-                        tmp = tmp/num;
-                        st.push(tmp);
-                        sign = nextSign;
-                        num = 0;
-                        break;
+            switch(sign)
+            {
+                case '+':
+                    st.push(num);
+                    break;
+                 case '-':
+                    st.push(-1*num);
+                    break;
+                 case '*': {
+                    int n = st.top(); st.pop();
+                    st.push(n*num);
+                    break;
+                 }
+                 case '/': {
+                    int n = st.top(); st.pop();
+                    st.push(n/num);
+                    break;
+                 }
+                default:
+                    cout<<"unknown op\n";
+                    // return -1;
+                    break;
             }
-            i++;
+            num=0;
+            sign = ch;
         }
         
         if(num>0) {
-            if (sign == '-') num = -1*num;
             st.push(num);
         }
-        
-        while(!st.empty()) {
-            res += st.top();
-            st.pop();
+        long res = 0;
+        while(!st.empty())
+        {
+            res += st.top(); st.pop();
         }
+        return (int)res;
         
-        return res;
     }
 };
+
+// sign = '+'
+// num 
+// stack<int> st
